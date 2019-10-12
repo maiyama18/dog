3c6b8324d243e2717363879de8b099848b883bfc
maiyama18 <myam0118@gmail.com>
2019-10-12 22:35:43 +0900

    parse let statement

--- parse/parse.go
+++ parse/parse.go
@@ -1,6 +1,8 @@
 package parse
 
 import (
+	"fmt"
+
 	"github.com/maiyama18/dog/ast"
 	"github.com/maiyama18/dog/lex"
 	"github.com/maiyama18/dog/token"
@@ -10,6 +12,8 @@ type Parser struct {
 	lexer        *lex.Lexer
 	currentToken token.Token
 	nextToken    token.Token
+
+	errors []error
 }
 
 func NewParser(lexer *lex.Lexer) *Parser {
@@ -21,11 +25,76 @@ func NewParser(lexer *lex.Lexer) *Parser {
 	return p
 }
 
+func (p *Parser) ParseProgram() *ast.Program {
+	var statements []ast.Statement
+
+	for !p.isCurrentTokenType(token.EOF) {
+		s := p.parseStatement()
+		if s != nil {
+			statements = append(statements, s)
+		}
+		p.consumeToken()
+	}
+
+	return &ast.Program{Statements: statements}
+}
+
+func (p *Parser) Errors() []error {
+	return p.errors
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.currentToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	tok := p.currentToken
+
+	if err := p.expectNextTokenType(token.IDENT); err != nil {
+		p.addError(err)
+		return nil
+	}
+	ident := &ast.Identifier{Token: p.currentToken, Name: p.currentToken.Literal}
+
+	if err := p.expectNextTokenType(token.ASSIGN); err != nil {
+		p.addError(err)
+		return nil
+	}
+
+	// TODO: parse expression
+	for !p.isCurrentTokenType(token.SEMICOLON) {
+		p.consumeToken()
+	}
+
+	return &ast.LetStatement{Token: tok, Identifier: ident}
+}
+
 func (p *Parser) consumeToken() {
 	p.currentToken = p.nextToken
 	p.nextToken = p.lexer.NextToken()
 }
 
-func (p *Parser) ParseProgram() *ast.Program {
+func (p *Parser) addError(err error) {
+	p.errors = append(p.errors, err)
+}
+
+func (p *Parser) isCurrentTokenType(tokenType token.Type) bool {
+	return p.currentToken.Type == tokenType
+}
+
+func (p *Parser) isNextTokenType(tokenType token.Type) bool {
+	return p.nextToken.Type == tokenType
+}
+
+func (p *Parser) expectNextTokenType(tokenType token.Type) error {
+	if !p.isNextTokenType(tokenType) {
+		return fmt.Errorf("expect token type %v, bug got %v", tokenType, p.currentToken)
+	}
+	p.consumeToken()
 	return nil
 }
---
+++ parse/parse_test.go
@@ -0,0 +1,50 @@
+package parse
+
+import (
+	"testing"
+
+	"github.com/maiyama18/dog/ast"
+
+	"github.com/maiyama18/dog/lex"
+)
+
+func TestLetStatements(t *testing.T) {
+	input := `
+let x = 5;
+let foo = 42;
+`
+
+	lexer := lex.NewLexer(input)
+	parser := NewParser(lexer)
+
+	program := parser.ParseProgram()
+
+	if len(parser.Errors()) > 0 {
+		t.Fatalf("got parser errors: %+v", parser.Errors())
+	}
+	if program == nil {
+		t.Fatalf("program is nil")
+	}
+	if len(program.Statements) != 2 {
+		t.Fatalf("program statments length wrong. want=%d, got=%d", 2, len(program.Statements))
+	}
+
+	expectedIdentNames := []string{"x", "foo"}
+
+	for i, s := range program.Statements {
+		testLetStatement(t, s, expectedIdentNames[i])
+	}
+}
+
+func testLetStatement(t *testing.T, statement ast.Statement, expectedIdentName string) {
+	t.Helper()
+
+	letStatement, ok := statement.(*ast.LetStatement)
+	if !ok {
+		t.Fatalf("not LetStatement: %+v", statement)
+	}
+
+	if letStatement.Identifier.Name != expectedIdentName {
+		t.Fatalf("identifier name wrong. want=%q, got=%q", expectedIdentName, letStatement.Identifier.Name)
+	}
+}
